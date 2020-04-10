bd3c9f9062ef3d4ba8929b82217f82cc6c01db07
Scott Kuehn <901698+skuehn@users.noreply.github.com>
2020-04-09 22:00:55 -0700

    add connection retries

--- connect.sh
+++ connect.sh
@@ -8,4 +8,22 @@ LOCAL_TUNNEL_PORT=8080
 SSH_KEY=$(terraform output ssh_key)
 VPN_HOST=$(terraform output instance_hostname)
 
-ssh -vv -oStrictHostKeyChecking=no -i ${SSH_KEY} -D ${LOCAL_TUNNEL_PORT} -C -q -N -l ubuntu ${VPN_HOST}
+function connect {
+    ssh -vv -oStrictHostKeyChecking=no -i ${SSH_KEY} -D ${LOCAL_TUNNEL_PORT} -C -q -N -l ubuntu ${VPN_HOST}    
+}
+
+ATTEMPT=1
+MAX_ATTEMPTS=3
+while [ ${ATTEMPT} -lt ${MAX_ATTEMPTS} ]; do
+    connect
+    if [ $? == 0 ]; then
+        echo "connection completed successfully. exiting" > /dev/stderr
+	exit 0
+    else
+        echo "connection failed sleeping 10 seconds and retrying. Attempt ${ATTEMPT} of ${MAX_ATTEMPTS}." > /dev/stderr
+	sleep 10s
+	ATTEMPT=$((ATTEMPT + 1))
+    fi
+done
+
+exit 1
